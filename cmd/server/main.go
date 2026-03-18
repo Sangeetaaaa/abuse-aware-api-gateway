59c694695ee0a9ceb906e12b92ce4067fe7f0951
Sangeetaaaa <sangeeta3112002@gmail.com>
2026-03-18 16:42:53 +0530

    removed unwanted code from main.go

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,59 +3,13 @@ package main
 import (
 	"fmt"
 	"os"
-	"strings"
-	"sync"
-	"time"
 	"todo-golang/internal/geo"
 	"todo-golang/internal/handlers"
 	"todo-golang/internal/middleware"
 	"todo-golang/internal/visitor"
 
 	"github.com/gin-gonic/gin"
-	"golang.org/x/time/rate"
 )
-
-type Visitor struct {
-	limiter            *rate.Limiter
-	lastSeen           time.Time
-	reputationScore    int
-	isBlocked          bool
-	isPermanentBlocked bool
-	blockUntil         time.Time
-	lastRateLimitTime  time.Time
-	notFoundCount      int
-}
-
-var visitors = make(map[string]*Visitor)
-var mu sync.Mutex
-
-func getVisitor(ip string) *Visitor {
-	mu.Lock()
-	defer mu.Unlock()
-
-	visitorStruct, exits := visitors[ip]
-	if !exits {
-		visitorStruct = &Visitor{
-			limiter:         rate.NewLimiter(1, 5),
-			reputationScore: 0, // Initialize IP score to 0
-		}
-		visitors[ip] = visitorStruct
-	}
-
-	visitorStruct.lastSeen = time.Now()
-
-	return visitorStruct
-}
-
-func isBot(ua string, pattern []string) bool {
-	ua = strings.ToLower(ua)
-	for _, bot := range pattern {
-		if strings.Contains(ua, strings.ToLower(bot)) {
-			return true
-		}
-	}
-	return false
-}
 
 func main() {
 
